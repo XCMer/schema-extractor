4559382b8a4819ed3fea94203c74a1d684ab159e
Raahul Seshadri <raahul@musictypes.com>
2013-06-13 18:20:39 +0530

    Reversed the parameter order of the extract function

--- src/Raahul/SchemaExtractor/SchemaExtractor.php
+++ src/Raahul/SchemaExtractor/SchemaExtractor.php
@@ -4,15 +4,15 @@ class SchemaExtractor
 {
     /**
      * Return an array of Column objects with descriptions of each column
-     * @param  string $type         The type of the database
      * @param  array  $descriptions An array of columns on which DESCRIBE has been run with PDO::FETCH_OBJ
+     * @param  string $type         The type of the database
      * @return array                An array of column objects
      */
-    public function extract($type = 'mysql', $descriptions)
+    public function extract($descriptions, $type = 'mysql')
     {
         if (!in_array( $type, array('mysql') ))
         {
-            throw new \Exception('Invalid database type selected.');
+            throw new \Exception('Invalid database type selected: ' . $type);
         }
 
         $columns = array();
